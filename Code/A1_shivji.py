731992feecd51c2343b4cec8496ecb7af42b343b
Aniket Mishra <info@bizoally.com>
2024-04-07 01:09:40 +0530

    sobel

--- Code/A1_shivji.py
+++ Code/A1_shivji.py
@@ -10,6 +10,10 @@ img_resize=cv2.resize(img, (h,w), interpolation=cv2.INTER_CUBIC)
 
 # blur_img = cv2.GaussianBlur(img_resize,ksize=(3,3),sigmaX=0,sigmaY=0)
 
+#sobel edge detection
+# sobelx= cv2.Sobel(src=img_resize, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+# sobely= cv2.Sobel(src=img_resize, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+
 
 
 cv2.imshow("shivji",img_resize)
